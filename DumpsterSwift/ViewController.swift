2655df152a4eddf6bb701af30f53264824aecbf5
Joel Wasserman <joel.wasserman8@gmail.com>
2015-02-17 16:05:53 -0500

    working homepage update

--- DumpsterSwift/ViewController.swift
+++ DumpsterSwift/ViewController.swift
@@ -9,9 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var homeLabel: UILabel!
+    @IBOutlet weak var gifView: UIWebView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    
+
+  
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        
+        homeLabel.font = UIFont(name: "Chalkduster", size:18)
+        startButton.titleLabel!.font = UIFont(name: "Chalkduster", size: 18)
+        
+        //gif animation code
+        let gifString = NSBundle.mainBundle().URLForResource("DumpLoopTrans2", withExtension: "gif")
+        let gif = NSData(contentsOfURL: gifString!)
+        gifView .loadData(gif, MIMEType:"image/gif", textEncodingName: nil, baseURL: nil)
+        gifView.userInteractionEnabled = false;
+        [self.view .addSubview(gifView)]
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
