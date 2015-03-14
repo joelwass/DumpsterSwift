e0be5f5c81d41295454afd6f376f0663fd0e5625
Joel Wasserman <joel.wasserman8@gmail.com>
2015-03-14 17:38:36 -0400

    added learnMoreViewController to start on learn more pages

---
+++ DumpsterSwift/LearnMoreViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LearnMoreViewController.swift
+//  DumpsterSwift
+//
+//  Created by Joel Wasserman on 3/14/15.
+//  Copyright (c) 2015 IVET. All rights reserved.
+//
+
+import UIKit
+
+
+class LearnMoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
