dab2429fdbcc2283d04a59a2850fe89483c8bd72
Joel Wasserman <joel.wasserman8@gmail.com>
2015-12-27 22:58:01 -0800

    working on facebook login

--- DumpsterSwift/AppDelegate.swift
+++ DumpsterSwift/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,9 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        // Override point for customization after application launch.
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     
         Parse.setApplicationId("BPYhaQES2x2riyAwbcmZVxtygMcefbVK3KjxSvzV", clientKey: "gsmozRPrRfbs35YwKkKCVUv7uhuRbeS95kB6Z3Qi")
         return true
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool
+    {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 
     func applicationWillResignActive(application: UIApplication) {
